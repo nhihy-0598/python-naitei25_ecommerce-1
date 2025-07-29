428a7ae132c153dfddfce3419e3dbce3a1d5c34a
NguyenKieuOanh-20225899 <nguyenkieuoanh17720041772004@gmail.com>
2025-07-30 04:11:15 +0700

    Tạo model CartOrder +CartOrderProducts+Category

--- core/models.py
+++ core/models.py
@@ -1,3 +1,101 @@
 from django.db import models
+from shortuuid.django_fields import ShortUUIDField
+from django.utils.html import mark_safe
+#from userauths.models import User
+from taggit.managers import TaggableManager
+from django_ckeditor_5.fields import CKEditor5Field
+from django.utils import timezone
 
-# Create your models here.
+
+
+STATUS_CHOICE = (
+    ("processing", "Processing"),
+    ("shipped", "Shipped"),
+    ("delivered", "Delivered"),
+)
+
+
+STATUS = (
+    ("draft", "Draft"),
+    ("disabled", "Disabled"),
+    ("rejected", "Rejected"),
+    ("in_review", "In Review"),
+    ("published", "Published"),
+)
+
+
+RATING = (
+    (1,  "★☆☆☆☆"),
+    (2,  "★★☆☆☆"),
+    (3,  "★★★☆☆"),
+    (4,  "★★★★☆"),
+    (5,  "★★★★★"),
+)
+#############
+class CartOrder(models.Model):
+    #user = models.ForeignKey(User, on_delete=models.CASCADE)
+    full_name = models.CharField(max_length=100, null=True, blank=True)
+    email = models.CharField(max_length=100, null=True, blank=True)
+    phone = models.CharField(max_length=100, null=True, blank=True)
+
+    address = models.CharField(max_length=100, null=True, blank=True)
+    city = models.CharField(max_length=100, null=True, blank=True)
+    state = models.CharField(max_length=100, null=True, blank=True)
+    country = models.CharField(max_length=100, null=True, blank=True)
+
+    price = models.DecimalField(max_digits=12, decimal_places=2, default="0.00")
+    saved = models.DecimalField(max_digits=12, decimal_places=2, default="0.00")
+    #coupons = models.ManyToManyField("core.Coupon", blank=True)
+    
+    shipping_method = models.CharField(max_length=100, null=True, blank=True)
+    tracking_id = models.CharField(max_length=100, null=True, blank=True)
+    tracking_website_address = models.CharField(max_length=100, null=True, blank=True)
+
+
+    paid_status = models.BooleanField(default=False)
+    order_date = models.DateTimeField(auto_now_add=True, null=True, blank=True)
+    product_status = models.CharField(choices=STATUS_CHOICE, max_length=30, default="processing")
+    sku = ShortUUIDField(null=True, blank=True, length=5,prefix="SKU", max_length=20, alphabet="1234567890")
+    oid = ShortUUIDField(null=True, blank=True, length=8, max_length=20, alphabet="1234567890")
+    stripe_payment_intent = models.CharField(max_length=1000, null=True, blank=True)
+    date = models.DateTimeField(default=timezone.now, null=True, blank=True)
+    
+    class Meta:
+        verbose_name_plural = "Cart Order"
+
+##############
+class CartOrderProducts(models.Model):
+    order = models.ForeignKey(CartOrder, on_delete=models.CASCADE)
+    invoice_no = models.CharField(max_length=200)
+    product_status = models.CharField(max_length=200)
+    item = models.CharField(max_length=200)
+    image = models.CharField(max_length=200)
+    qty = models.IntegerField(default=0)
+    price = models.DecimalField(max_digits=12, decimal_places=2, default="0.00")
+    total = models.DecimalField(max_digits=12, decimal_places=2, default="0.00")
+
+    class Meta:
+        verbose_name_plural = "Cart Order Items"
+
+    def order_img(self):
+        return mark_safe('<img src="/media/%s" width="50" height="50" />' % (self.image))
+
+##############
+class Category(models.Model):
+    cid = ShortUUIDField(unique=True, length=10, max_length=20,
+                         prefix="cat", alphabet="abcdefgh12345")
+    title = models.CharField(max_length=100, default="Food")
+    image = models.ImageField(upload_to="category", default="category.jpg")
+
+    class Meta:
+        verbose_name_plural = "Categories"
+
+    def category_image(self):
+        return mark_safe('<img src="%s" width="50" height="50" />' % (self.image.url))
+
+    def product_count(self):
+        return Product.objects.filter(category=self).count()
+
+    def __str__(self):
+        return self.title
+
