df25ad2e466d6d9a11b5005b7816e14ad61913a3
oanhntk-5899 <nguyenkieuoanh17720041772004@gmail.com>
2025-07-30 14:04:11 +0700

    xây dưng userauth+Category+CartOrder+CartOrderProducts (#3)

--- core/models.py
+++ core/models.py
@@ -1,8 +1,27 @@
 from django.db import models
-from django.contrib.auth.models import User
 from shortuuid.django_fields import ShortUUIDField
+from django.utils.html import mark_safe
+from django.conf import settings
+from taggit.managers import TaggableManager
+from django_ckeditor_5.fields import CKEditor5Field
+from django.utils import timezone
 
 # Create your models here.
+STATUS_CHOICE = (
+    ("processing", "Processing"),
+    ("shipped", "Shipped"),
+    ("delivered", "Delivered"),
+)
+
+
+STATUS = (
+    ("draft", "Draft"),
+    ("disabled", "Disabled"),
+    ("rejected", "Rejected"),
+    ("in_review", "In Review"),
+    ("published", "Published"),
+)
+
 
 RATING = (
     (1,  "★☆☆☆☆"),
@@ -23,7 +42,7 @@ class Vendor(models.Model):
     authentic_rating = models.CharField(max_length=50)
     days_return = models.CharField(max_length=50)
     warranty_period = models.CharField(max_length=50)
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     date = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
@@ -50,7 +69,7 @@ class Coupon(models.Model):
 
 class CouponUser(models.Model):
     coupon = models.ForeignKey(Coupon, on_delete=models.CASCADE)
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
 
     class Meta:
         db_table = 'coupon_user'
@@ -64,7 +83,14 @@ class Category(models.Model):
 
     class Meta:
         verbose_name_plural = "Categories"
+    def category_image(self):
+        return mark_safe('<img src="%s" width="50" height="50" />' % (self.image.url))
 
+    def product_count(self):
+        return Product.objects.filter(category=self).count()
+
+    def __str__(self):
+        return self.title
 
 class Product(models.Model):
     pid = ShortUUIDField(unique=True, length=10, max_length=20, alphabet="abcdefgh12345", primary_key=True)
@@ -105,7 +131,7 @@ class Product(models.Model):
         verbose_name_plural = "Products"
 
 class ProductReview(models.Model):
-    user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.SET_NULL, null=True)
     product = models.ForeignKey(Product, on_delete=models.SET_NULL, null=True, related_name="reviews")
     review = models.TextField()
     rating = models.IntegerField(choices=RATING, default=None)
@@ -125,7 +151,7 @@ class ReturnRequest(models.Model):
         ('completed', 'Completed'),
     )
 
-    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='return_requests')
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='return_requests')
     order_product = models.ForeignKey('CartOrderProducts', on_delete=models.CASCADE, related_name='return_requests')
     reason = models.TextField()
     status = models.CharField(max_length=50, choices=STATUS_CHOICES, default='pending')
@@ -133,4 +159,52 @@ class ReturnRequest(models.Model):
 
     def __str__(self):
         return f"ReturnRequest #{self.pk} by {self.user.username}"
+    
+class CartOrder(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    full_name = models.CharField(max_length=100, null=True, blank=True)
+    email = models.CharField(max_length=100, null=True, blank=True)
+    phone = models.CharField(max_length=100, null=True, blank=True)
+
+    address = models.CharField(max_length=100, null=True, blank=True)
+    city = models.CharField(max_length=100, null=True, blank=True)
+    state = models.CharField(max_length=100, null=True, blank=True)
+    country = models.CharField(max_length=100, null=True, blank=True)
+
+    price = models.DecimalField(max_digits=12, decimal_places=2, default="0.00")
+    saved = models.DecimalField(max_digits=12, decimal_places=2, default="0.00")
+    coupons = models.ManyToManyField("core.Coupon", blank=True)
+    
+    shipping_method = models.CharField(max_length=100, null=True, blank=True)
+    tracking_id = models.CharField(max_length=100, null=True, blank=True)
+    tracking_website_address = models.CharField(max_length=100, null=True, blank=True)
+
+
+    paid_status = models.BooleanField(default=False)
+    order_date = models.DateTimeField(auto_now_add=True, null=True, blank=True)
+    product_status = models.CharField(choices=STATUS_CHOICE, max_length=30, default="processing")
+    sku = ShortUUIDField(null=True, blank=True, length=5,prefix="SKU", max_length=20, alphabet="1234567890")
+    oid = ShortUUIDField(null=True, blank=True, length=8, max_length=20, alphabet="1234567890")
+    stripe_payment_intent = models.CharField(max_length=1000, null=True, blank=True)
+    date = models.DateTimeField(default=timezone.now, null=True, blank=True)
+    
+    class Meta:
+        verbose_name_plural = "Cart Order"
+
+
+class CartOrderProducts(models.Model):
+    order = models.ForeignKey(CartOrder, on_delete=models.CASCADE)
+    invoice_no = models.CharField(max_length=200)
+    product_status = models.CharField(max_length=200)
+    item = models.CharField(max_length=200)
+    image = models.CharField(max_length=200)
+    qty = models.IntegerField(default=0)
+    price = models.DecimalField(max_digits=12, decimal_places=2, default="0.00")
+    total = models.DecimalField(max_digits=12, decimal_places=2, default="0.00")
+
+    class Meta:
+        verbose_name_plural = "Cart Order Items"
+
+    def order_img(self):
+        return mark_safe('<img src="/media/%s" width="50" height="50" />' % (self.image))
 
--- ecomprj/settings.py
+++ ecomprj/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     'core',
+    'userauths'
 ]
 
 MIDDLEWARE = [
@@ -132,3 +133,4 @@ STATICFILES_DIRS = [os.path.join(BASE_DIR, 'staticfiles')]
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+AUTH_USER_MODEL = 'userauths.User'
