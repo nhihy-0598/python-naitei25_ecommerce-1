735d1b02aa3f4c7f11bd0ff576a81aa336f08aa4
Pham Viet Anh <166052827+PhamVanh04@users.noreply.github.com>
2025-08-04 12:44:27 +0700

    Templates: cart and review

--- core/urls.py
+++ core/urls.py
@@ -16,4 +16,11 @@ urlpatterns = [
     path("dashboard/order/<id>/", order_detail, name="order-detail"),
     path("category/", category_list_view),
     path("category/<cid>/", category_product_list__view, name="category-product-list"),
+     # Homepage
+    path("", index, name="index"),
+    path("cart/", cart_view, name="cart"),
+    path("add-to-cart/", add_to_cart, name="add-to-cart"),
+    path("delete-from-cart/", delete_item_from_cart, name="delete-from-cart"),
+    path("update-cart/", update_cart, name="update-cart"),
+    path("ajax-add-review/<int:pid>/", ajax_add_review, name="ajax-add-review"),
 ]
